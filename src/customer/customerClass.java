a8f4c53ced83a779ae9c1831edae0105e6d037a9
WaryGarry <kyjkyj9368@naver.com>
2024-11-08 01:34:21 +0900

    업댓 새루 함

--- src/customer/customerClass.java
+++ src/customer/customerClass.java
@@ -1,5 +1,7 @@
 package customer;
 
 public class customerClass {
-    System.out.println("Hello World"!);
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
