a8706a2659a110c271dc256c3b4d3eb1a4ac5bfc
eunbeeyaaa <jy02119@naver.com>
2024-11-14 17:13:45 +0900

    customerclass

--- src/customer/customerClass.java
+++ src/customer/customerClass.java
@@ -1,4 +1,34 @@
 package customer;
 
-public class customerClass {
+public class CustomerClass {
+
+    private String name;
+    private RecipeClass requestedRecipe;
+    private long requestTime;
+
+    // 생성자
+
+    public CustomerClass(String name, RecipeClass requestedRecipe) {
+        this.name = name;
+        this.requestedRecipe = requestedRecipe;
+        this.requestTime = System.currentTimeMillis(); // 고객이 등장한 시간
+
+    }
+
+    // 요청한 요리 반환
+    public RecipeClass getRequestedRecipe() {
+        return requestedRecipe;
+    }
+
+    // 고객의 이름 반환
+    public String getName() {
+        return name;
+    }
+
+    // 고객이 요청한 요리 확인
+    public boolean isCorrectOrder(RecipeClass providedRecipe) {
+        return this.requestedRecipe.equals(providedRecipe);
+    }
+
+
 }
