bd9526847f0f1b936395ec38fdd225ca9fdfbbbe
eunbeeyaaa <jy02119@naver.com>
2024-12-03 14:12:09 +0900

    recipe생성&customer수정
    레시피들 recipe클래스에 정리
    customerclass클래스에 손님추가,레시피배치

---
+++ src/customer/RecipeClass.java
@@ -0,0 +1,59 @@
+package customer;
+import java.util.Random;
+// 추상 클래스: 레시피
+public abstract class RecipeClass {
+    protected String name;
+    protected String[] ingredients;
+
+    // 음식 이름과 재료 반환
+    public String getName() {
+        return name;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public String[] getIngredientWords() { // 레시피의 재료당 랜덤 단어 배치하여 배열로 반환
+
+        String[] randomWords = {
+                "말", "획득하다", "싸우다", "찾다", "기다리다", "기억하다",
+                "지다", "달리다", "떠나다", "꿈", "이상"
+        };
+
+        Random random = new Random();
+        String[] result = new String[ingredients.length];
+
+        for (int i = 0; i < ingredients.length; i++) {
+            result[i] = randomWords[random.nextInt(randomWords.length)];
+        }
+
+        return result;
+    }
+}
+
+// 햄버거 레시피 클래스 (RecipeClass 상속)
+public class HamburgerRecipe extends RecipeClass {
+    public HamburgerRecipe() {
+        this.name = "햄버거";
+        this.ingredients = new String[]{"참깨빵", "고기 패티", "특별한 소스", "양상추", "양파"};
+    }
+}
+
+// 라면 레시피 클래스 (RecipeClass 상속)
+public class RamenRecipe extends RecipeClass {
+    public RamenRecipe() {
+        this.name = "라면";
+        this.ingredients = new String[]{"뜨거운 물", "소스", "면", "계란", "김치"};
+    }
+}
+
+// 타코야끼 레시피 클래스 (RecipeClass 상속)
+public class TakoyakiRecipe extends RecipeClass {
+    public TakoyakiRecipe() {
+        this.name = "타코야끼";
+        this.ingredients = new String[]{"반죽", "문어", "콘옥수수", "소스", "가쓰오부시"};
+    }
+}
+
+// 다른 레시피도 마찬가지로 작성
--- src/customer/customerClass.java
+++ src/customer/customerClass.java
@@ -1,34 +1,78 @@
 package customer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class CustomerClass {
 
     private String name;
     private RecipeClass requestedRecipe;
-    private long requestTime;
 
-    // 생성자
+    // 손님 목록을 여기서 관리
+    private static List<CustomerClass> customers = new ArrayList<>();
+    private static Random random = new Random();
 
-    public CustomerClass(String name, RecipeClass requestedRecipe) {
-        this.name = name;
+    // 고객이 주문한 요리
+    public CustomerClass(RecipeClass requestedRecipe) {
         this.requestedRecipe = requestedRecipe;
-        this.requestTime = System.currentTimeMillis(); // 고객이 등장한 시간
-
     }
 
-    // 요청한 요리 반환
     public RecipeClass getRequestedRecipe() {
         return requestedRecipe;
     }
 
-    // 고객의 이름 반환
-    public String getName() {
-        return name;
-    }
-
-    // 고객이 요청한 요리 확인
     public boolean isCorrectOrder(RecipeClass providedRecipe) {
-        return this.requestedRecipe.equals(providedRecipe);
+        return this.requestedRecipe.isCorrectOrder(providedRecipe);
     }
 
 
+    Thread customerThread = new Thread(() -> {
+        while (getCustomers().size() < 4) {
+            //Thread.sleep(4000);  // 4초마다 손님 등장
+            try {
+                Thread.sleep(4000);  // 4초마다 손님 등장
+            } catch (InterruptedException e) {
+                e.printStackTrace(); // 예외 발생 시 예외 정보 출력
+            }
+
+            if (getCustomers().size() < 4) {
+                // 손님을 랜덤으로 생성하여 추가
+                addCustomer();
+            }
+        }
+    });
+
+    // 손님을 추가하는 메서드
+    public static void addCustomer() {
+        if (customers.size() < 4) {
+
+            RecipeClass randomRecipe = getRandomRecipe(); // 요리 랜덤 선택 (햄버거, 라면, 타코야끼 중 하나)
+
+            CustomerClass newCustomer = new CustomerClass(randomRecipe);
+            customers.add(newCustomer);
+            // 레시피 입력하면 손님 나가는 것 추가로 구현해야 함
+        }
+    }
+
+    // 손님 목록 반환
+    public static List<CustomerClass> getCustomers() {
+        return customers;
+    }
+
+    // 새 손님 랜덤으로 음식 선택
+    private static RecipeClass getRandomRecipe() {
+
+        int index = random.nextInt(3);  // 0: 햄버거, 1: 라면, 2: 타코야끼
+        switch (index) {
+            case 0:
+                return new HamburgerRecipe();
+            case 1:
+                return new RamenRecipe();
+            case 2:
+                return new TakoyakiRecipe();
+            default:
+                return null;
+        }
+    }
 }
